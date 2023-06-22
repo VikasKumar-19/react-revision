a51a197c6e9f7bf44a8130024970c3f02ecc1129
Vikas Kumar <vk.pratap98@gmail.com>
2023-06-22 22:46:59 +0530

    Food app layout structure prepared

---
+++ lecture-4/App.js
@@ -0,0 +1,42 @@
+//We cannot directly import or export in common script files
+//we need to tell the browser that this is a type module script file
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () => {
+  return (
+    <div>
+      <img
+      width={85}
+        alt="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX0oZvf_Vj2VpP9fp5NLte238mjaO3LScZag&usqp=CAU"
+      />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+const Body = () => {
+  return <div>body</div>;
+};
+const AppLayout = () => {
+  return (
+    <>
+      <Heading />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
